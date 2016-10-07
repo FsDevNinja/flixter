67df76325b47770fd7b8233d9411f6293b11f0b0
Matt Taylor <makproductions06@gmail.com>
2016-10-07 00:28:34 -0400

    lesson creation error

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,10 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
-    resources :courses, only: [:new, :create,:update, :show] do
+    resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
   end
